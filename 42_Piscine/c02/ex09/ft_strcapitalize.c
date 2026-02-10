7f5fa39d65c98a36e3b0b21817c07ed868da2bdc
Pablo Monteiro <pablo_monteiro@hotmail.com>
2026-02-10 13:23:01 +0100

    Add files via upload

---
+++ 42_Piscine/c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,43 @@
+
+
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 || (!(str[i - 1] >= 'a' && str[i - 1] <= 'z')
+			&& !(str[i - 1] >= 'A' && str[i - 1] <= 'Z')
+			&& !(str[i - 1] >= '0' && str[i - 1] <= '9')))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (str);
+}
+#include <stdio.h>
+
+char	*ft_strcapitalize(char *str);
+
+int	main(void)
+{
+	char	str1[] = "hola";
+	char	str2[] = "HoLa 42! mundo-TEST";
+	char	str3[] = "";
+	char	str4[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+
+	printf("%s\n", ft_strcapitalize(str1));
+	printf("%s\n", ft_strcapitalize(str2));
+	printf("%s\n", ft_strcapitalize(str3));
+	printf("%s\n", ft_strcapitalize(str4));
+	return (0);
+}
