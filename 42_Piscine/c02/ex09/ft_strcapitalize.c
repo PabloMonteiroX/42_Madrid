1913c36cac60db274f6941426f5ef81dc6caed23
Pablo Monteiro <pablo_monteiro@hotmail.com>
2026-02-20 08:36:11 +0100

    Update ft_strcapitalize.c

--- 42_Piscine/c02/ex09/ft_strcapitalize.c
+++ 42_Piscine/c02/ex09/ft_strcapitalize.c
@@ -1,43 +1,82 @@
+#include <unistd.h>
 
+int	ft_is_lower(char c)
+{
+	return (c >= 'a' && c <= 'z');
+}
 
+int	ft_is_upper(char c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
+int	ft_is_alpha(char c)
+{
+	return (ft_is_lower(c) || ft_is_upper(c));
+}
+
+int	ft_is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_is_alnum(char c)
+{
+	return (ft_is_alpha(c) || ft_is_digit(c));
+}
+
+int	ft_is_word_start(char *str, int i)
+{
+	if (i == 0)
+		return (1);
+	if (!ft_is_alnum(str[i - 1]))
+		return (1);
+	return (0);
+}
 
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (i == 0 || (!(str[i - 1] >= 'a' && str[i - 1] <= 'z')
-			&& !(str[i - 1] >= 'A' && str[i - 1] <= 'Z')
-			&& !(str[i - 1] >= '0' && str[i - 1] <= '9')))
+		if (ft_is_word_start(str, i))
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
+			if (ft_is_lower(str[i]))
+				str[i] -= 32;
 		}
 		else
 		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
+			if (ft_is_upper(str[i]))
+				str[i] += 32;
 		}
 		i++;
 	}
 	return (str);
 }
-#include <stdio.h>
+
+#include <unistd.h>
 
 char	*ft_strcapitalize(char *str);
 
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
 int	main(void)
 {
-	char	str1[] = "hola";
-	char	str2[] = "HoLa 42! mundo-TEST";
-	char	str3[] = "";
-	char	str4[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
 
-	printf("%s\n", ft_strcapitalize(str1));
-	printf("%s\n", ft_strcapitalize(str2));
-	printf("%s\n", ft_strcapitalize(str3));
-	printf("%s\n", ft_strcapitalize(str4));
+	ft_putstr(ft_strcapitalize(str));
+	write(1, "\n", 1);
 	return (0);
 }
